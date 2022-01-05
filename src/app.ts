5ded2b53d42ccb7c6c035e5ccae30b7ecf0fd357
maya <maya.salcedo07@gmail.com>
2022-01-05 13:38:39 +0200

    more classes and custom types

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,48 @@
+// Project Type
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
+// Project State Management
+type Listener = (items: Project[]) => void;
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
-  private static instance: ProjectState:
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
+  private static instance: ProjectState;
 
-  private constructor() {
-
-  }
+  private constructor() {}
 
   static getInstance() {
-    if(this.instance) {
-      return this.instance
+    if (this.instance) {
+      return this.instance;
     }
     this.instance = new ProjectState();
     return this.instance;
   }
 
-  addListener(listenerFn: Function){
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople,
-    };
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Active
+    );
+
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
@@ -98,7 +115,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById(
@@ -114,21 +131,23 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
-    })
+    });
 
     this.attach();
     this.renderContent();
   }
 
-  private renderProjects(){
-    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
-    for ( const projItem of this.assignedProjects){
-      const listItem = document.createElement("li");
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    for (const projItem of this.assignedProjects) {
+      const listItem = document.createElement('li');
       listItem.textContent = projItem.title;
-      listEl.appendChild(listItem)
+      listEl.appendChild(listItem);
     }
   }
 
