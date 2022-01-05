642f5649b230b7580e8ca7c264df4bca60a614b3
maya <maya.salcedo07@gmail.com>
2022-01-05 11:12:59 +0200

    rendering project lists

--- dist/app.js
+++ dist/app.js
@@ -42,6 +42,28 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+//ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 //ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -108,3 +130,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const projInput = new ProjectInput();
+const activeProjList = new ProjectList('active');
+const finishedProjList = new ProjectList('finished');
